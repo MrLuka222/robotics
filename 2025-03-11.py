1158992feee435b8e08745c8e110db11a61847db
MrLuka222 <LukaG538@stemcollegiate.ca>
2025-03-11 14:08:49 -0600

    Create 2025-03-11.py

---
+++ 2025-03-11.py
@@ -0,0 +1,6 @@
+# Today I learned how to make your program have more than one response to different things
+Hero = input("Enter the name of your hero ")
+if Hero == "Batman":
+  print("mine too")
+else:
+  print("Excellent choice.")
